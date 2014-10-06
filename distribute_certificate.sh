4989730f949bee2a4a66921228e8bf53b7b8920a
Jim Cronqvist <jim.cronqvist@gmail.com>
2014-10-06 10:48:06 +0700

    Create distribute_certificate.sh

---
+++ distribute_certificate.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for var in "$@"
+do
+	ssh-copy-id -i ~/.ssh/DIAKRIT_id_rsa.pub "ubuntu@"$var && ssh -t $var "sudo bash -c \"echo 'ubuntu ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers.d/ubuntu\""
+done
