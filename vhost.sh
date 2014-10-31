98dad878a0a732b96ba62a416252b7affce4da08
Jim Cronqvist <jim.cronqvist@gmail.com>
2014-10-31 11:48:47 +0700

    Update vhost.sh

--- vhost.sh
+++ vhost.sh
@@ -14,11 +14,18 @@ echo "This script needs to be ran from a user with root permissions.";
 fi
 
 # Check for 2 passed arguments, otherwise abort.
-if [ $# != 2 ] ; then
+if [ $# -lt 2 ] ; then
     echo "You have not passed the correct number of arguments. This script should be used with the following syntax:"
     echo "sudo bash vhost.sh example.com /var/www/example.com"
+    echo "or with SSL:"
+    echo "sudo bash vhost.sh example.com /var/www/example.com --ssl"
     echo ""
     exit 1;
+fi
+
+SSL=0
+if echo $* | grep -e " --ssl" -q ; then
+    SSL=1
 fi
 
 virtual_host="<VirtualHost *:80>
