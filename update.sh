779e77eebe6cf292944dfc090d5b0ccd34a58ee5
Jim Cronqvist <jim.cronqvist@gmail.com>
2015-03-15 23:45:39 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,5 +2,6 @@
 
 for var in "$@"
 do
-    ssh -t $var "sudo apt-get dist-upgrade && grep -s -q 'Vendor: VMware' /proc/scsi/scsi && ! test -e /var/run/vmtoolsd.pid && sudo /usr/bin/vmware-config-tools.pl -d || echo -e '\nVmware tools does not need to be re-compiled\n'"
+    echo -e "$(tput setaf 2)\nConnecting to $var \n $(tput sgr0)"
+    ssh -t $var "sudo apt-get dist-upgrade && grep -s -q 'Vendor: VMware' /proc/scsi/scsi && ! test -e /var/run/vmtoolsd.pid && sudo /usr/bin/vmware-config-tools.pl -d || echo -e 'Vmware tools does not need to be re-compiled\n'
 done
