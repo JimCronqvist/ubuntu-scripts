e2e02c1fcf562076220c98286834e784b227790c
Jim Cronqvist <jim.cronqvist@gmail.com>
2015-05-24 23:09:54 +0700

    Update mysqldump.sh

--- mysqldump.sh
+++ mysqldump.sh
@@ -6,4 +6,16 @@ DB_HOST="localhost"
 DB_USER="backup"
 DB_PASS="password"
 
-mysqldump --single-transaction --master-data --complete-insert --events -h $DB_HOST -u $DB_USER -p$DB_PASS --all-databases > $DB_HOST"_"`date +%Y-%m-%d_%H.%M.%S.sql`
+# Keep the SQL dumps for X days, 0 = forever
+KEEP_DAYS=0
+BACKUP_FOLDER="/home/ubuntu/mysqldump/"
+
+
+mkdir -p "$BACKUP_FOLDER"
+
+# Delete old backup files.
+if [ $KEEP_DAYS -gt 0 ]; then
+    find "$BACKUP_FOLDER"* -maxdepth 0 -type f -mtime +$KEEP_DAYS -iname "$DB_HOST*.sql" -print -delete
+fi
+
+mysqldump --single-transaction --master-data --complete-insert --events -h $DB_HOST -u $DB_USER -p$DB_PASS --all-databases > "$BACKUP_FOLDER$DB_HOST""_"`date +%Y-%m-%d_%H.%M.%S.sql`
