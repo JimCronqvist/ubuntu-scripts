b4ca404f43e3a44a4da576261d6a5160522511c5
Jim Cronqvist <jim.cronqvist@gmail.com>
2015-12-10 19:10:20 +0700

    Update vhost.sh

--- vhost.sh
+++ vhost.sh
@@ -3,8 +3,36 @@
 ###################################################
 ## Automatic Apache2 virtual host install script ##
 ## Jim Cronqvist <jim.cronqvist@gmail.com>       ##
-## Updated: 2014-05-06                           ##
+## Updated: 2015-12-10                           ##
 ###################################################
+
+# Confirm function that will be used later for yes and no questions.
+Confirm () {
+    while true; do
+        if [ "${2:-}" = "Y" ]; then
+            prompt="Y/n"
+            default=Y
+        elif [ "${2:-}" = "N" ]; then
+            prompt="y/N"
+            default=N
+        else
+            prompt="y/n"
+            default=
+        fi
+
+        read -p "${1:-Are you sure?} [$prompt]: " reply
+
+        #Default?
+        if [ -z "$reply" ]; then
+            reply=$default
+        fi
+
+        case ${reply:-$2} in
+            [Yy]* ) return 0;;
+            [Nn]* ) return 1;;
+        esac
+    done
+}
 
 
 # Abort if not root.
@@ -82,13 +110,20 @@ if [ $SSL -eq 1 ]; then
     cat /etc/apache2/sites-available/$1.ssl.conf
     
     if [ -f  "/etc/apache2/ssl/$1.key" ]; then
-    	a2ensite $1.ssl.conf
-    	service apache2 reload
+        a2ensite $1.ssl.conf
+        service apache2 reload
     else
-    	echo ""
-        echo "The SSL certificate was not found, please make sure that the files exist before you enable this site. Ones that is done, enable it with the command:"
-        echo "sudo a2ensite $1.ssl.conf && sudo service apache2 reload"
-        echo ""
+        if Confirm "The SSL certificate was not found, do you want to create a self signed certificate?" Y; then
+            sudo mkdir /etc/apache2/ssl
+            sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/apache2/ssl/$1.key -out /etc/apache2/ssl/ssl_certificate.crt -subj "/C=/ST=/L=/O=/CN= "
+            sudo cp /etc/apache2/ssl/ssl_certificate.crt /etc/apache2/ssl/IntermediateCA.crt
+            sudo a2ensite $1.ssl.conf && sudo service apache2 reload
+        else
+            echo ""
+            echo "The SSL certificate was not found, please make sure that the files exist before you enable this site. Ones that is done, enable it with the command:"
+            echo "sudo a2ensite $1.ssl.conf && sudo service apache2 reload"
+            echo ""
+        fi
     fi
 else
     sudo bash -c "echo '$virtual_host' > /etc/apache2/sites-available/$1.conf"
