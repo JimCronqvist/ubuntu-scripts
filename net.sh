39e4f47cac1c5e8f016984c9177ebb113cec5bfa
Jim Cronqvist <jim.cronqvist@gmail.com>
2016-07-11 10:55:09 +0200

    Create net.sh

---
+++ net.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+echo Get network summary
+ss -s
+
+echo Count of all connections
+netstat -an | wc -l
+
+echo Count of all connections on a specific port
+netstat -an | grep :443 | wc -l
+
+echo Count of all categories of connections, such as TIME_WAIT, LISTEN, etc.
+netstat -ant | awk '{print $6}' | sort | uniq -c | sort -n
