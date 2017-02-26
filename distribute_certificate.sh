2cca367b46a525239b0a22d918887e026e8dbd33
Jim Cronqvist <jim.cronqvist@gmail.com>
2017-02-26 19:15:25 +0700

    Update distribute_certificate.sh

--- distribute_certificate.sh
+++ distribute_certificate.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
+COPY_IDENTITY_FILE="/home/ubuntu/.ssh/id_rsa.pub"
+
 for var in "$@"
 do
-	ssh-copy-id -i ~/.ssh/id_rsa.pub "ubuntu@"$var && ssh -t $var "sudo bash -c \"echo 'ubuntu ALL=(ALL) NOPASSWD:ALL' | ( umask 337; cat >> /etc/sudoers.d/ubuntu; )\""
+    cat $COPY_IDENTITY_FILE | ssh -t $var 'umask 0077; mkdir -p .ssh; cat >> .ssh/authorized_keys && echo "Key copied" || echo "Key not copied"'
+    
+    # Optional if you want to enable sudo without password - if not, comment it away
+    ssh -t $var "sudo bash -c \"echo 'ubuntu ALL=(ALL) NOPASSWD:ALL' | ( umask 337; cat >> /etc/sudoers.d/ubuntu; )\""
 done
