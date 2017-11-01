13ce299f12fcdfc428cc15c3b6071825db135ab4
Jim Cronqvist <jim.cronqvist@gmail.com>
2017-11-01 15:04:51 +0700

    Update memory_usage.sh

--- memory_usage.sh
+++ memory_usage.sh
@@ -14,3 +14,19 @@ ps -eo rss,pid,user,command | sort -rn | awk '$1 > 10000' | awk '
     printf ("\n")
 }
  ' | sed '1!G;h;$!d'
+
+echo ""
+echo ""
+
+if [ ! -f /usr/bin/ps_mem.py ]; then
+    sudo wget -O /usr/bin/ps_mem.py https://raw.githubusercontent.com/pixelb/ps_mem/master/ps_mem.py && chmod +x /usr/bin/ps_mem.py
+fi
+
+/usr/bin/ps_mem.py
+
+echo ""
+echo ""
+
+free -m | awk 'NR==2{printf "Memory Usage: %.2fGB/%.2fGB (%.2f%%)\n", $3/1024,$2/1024,$3/$2*100 }'
+
+echo ""
