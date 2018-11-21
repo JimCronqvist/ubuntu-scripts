b20f2fd0c60bf3e5af90e8dddd1baa552fde6188
Jim Cronqvist <jim.cronqvist@gmail.com>
2018-11-21 18:58:55 +0700

    Update install-docker-host.sh

--- install-docker-host.sh
+++ install-docker-host.sh
@@ -270,7 +270,7 @@ EOF
             curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
             unzip awscli-bundle.zip
             sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
-	    rm awscli-bundle.zip -f && rm ./awscli-bundle/ -rf
+            rm awscli-bundle.zip -f && rm ./awscli-bundle/ -rf
             #aws configure
             
             ;;
