cb4b73cc3abf4d8c11f158c9afc911f585cedca4
Jim Cronqvist <jim.cronqvist@gmail.com>
2019-06-18 17:35:39 +0700

    Update mysql-backup.sh

--- mysql-backup.sh
+++ mysql-backup.sh
@@ -82,7 +82,7 @@ do
 
     PREFIX="${BACKUP_DIR}/${HOSTNAME}.${db}."
     NUM_BACKUPS=$(ls -l "${PREFIX}"* | wc -l)
-    if [ $NUM_BACKUPS > $KEEP_BACKUPS ]; then
+    if [ $NUM_BACKUPS -gt $KEEP_BACKUPS ]; then
         NUM_REMOVE=$[NUM_BACKUPS - $KEEP_BACKUPS]
         REMLIST=$(ls -ctr "${PREFIX}"* | head -n ${NUM_REMOVE})
         for i in $REMLIST
