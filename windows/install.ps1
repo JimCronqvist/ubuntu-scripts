0ec1b2370423aa5acac291c50d4ccf4fe939cdd0
Jim Cronqvist <jim.cronqvist@gmail.com>
2022-12-04 11:49:37 +0100

    Update install.ps1

--- windows/install.ps1
+++ windows/install.ps1
@@ -25,9 +25,13 @@ $computerName = Read-Host 'Enter New Computer Name'
 Write-Host "Renaming this computer to: " $computerName  -ForegroundColor Yellow
 Rename-Computer -NewName $computerName
 
+# Modify some Windows behaviors
+## Disable "Show more options" context menu
+reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
+
 # Power options
 Write-Host ""
-Write-Host "Disable Sleep on AC Power..." -ForegroundColor Green
+Write-Host "Change the power plan, disable sleep and turn off screens after 15 minutes when on AC power..." -ForegroundColor Green
 Write-Host "------------------------------------" -ForegroundColor Green
 Powercfg /Change monitor-timeout-ac 15
 Powercfg /Change standby-timeout-ac 0
@@ -48,8 +52,12 @@ foreach ($uwp in $uwpRubbishApps) {
 Write-Host ""
 Write-Host "Enabling Hyper-V (required for wsl2) and Telnet Client..." -ForegroundColor Green
 Write-Host "------------------------------------" -ForegroundColor Green
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All # Is VirtualMachinePlatform enought for wsl2?
 Enable-WindowsOptionalFeature -Online -FeatureName TelnetClient -All
+
+# Install WSL2
+# ...
+
 
 # Enable Windows Developer Mode
 #Write-Host ""
@@ -68,6 +76,7 @@ else {
     Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
+# Install applications via Choco
 Write-Host ""
 Write-Host "Installing Choco Applications..." -ForegroundColor Green
 Write-Host "------------------------------------" -ForegroundColor Green
@@ -75,9 +84,21 @@ Write-Host "------------------------------------" -ForegroundColor Green
 $Apps = @(
     "7zip.install",
     "googlechrome",
+    "firefox"
     "vlc",
     "notepadplusplus.install",
-    "nodejs-lts",
+    "lightshot.install",
+    "dropbox",
+    "jetbrainstoolbox",
+    "authy-desktop",
+    "openvpn-connect",
+    "icue",
+    "hwinfo",
+    "spotify",
+    
+    #"docker-desktop",
+    
+    #"nodejs-lts",
     #"postman",
     #"sysinternals",
     #"powershell-core",
@@ -86,16 +107,14 @@ $Apps = @(
 foreach ($app in $Apps) {
     choco install $app -y
 }
-
 # Install 'gsudo'
 PowerShell -Command "Set-ExecutionPolicy RemoteSigned -scope Process; [Net.ServicePointManager]::SecurityProtocol = 'Tls12'; iwr -useb https://raw.githubusercontent.com/gerardog/gsudo/master/installgsudo.ps1 | iex"
 
-Write-Host "Applying file explorer settings..." -ForegroundColor Green
+Write-Host "Apply Windows Explorer settings..." -ForegroundColor Green
 cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v HideFileExt /t REG_DWORD /d 0 /f"
-#cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v AutoCheckSelect /t REG_DWORD /d 0 /f"
-#cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v LaunchTo /t REG_DWORD /d 1 /f"
+cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v Hidden /t REG_DWORD /d 1 /f"
 
-Write-Host "Excluding repos from Windows Defender..." -ForegroundColor Green
+Write-Host "Exclude repos from Windows Defender..." -ForegroundColor Green
 Add-MpPreference -ExclusionPath "$env:USERPROFILE\www"
 
 #Write-Host "Enabling Hardware-Accelerated GPU Scheduling..." -ForegroundColor Green
@@ -103,10 +122,10 @@ Add-MpPreference -ExclusionPath "$env:USERPROFILE\www"
 
 # -----------------------------------------------------------------------------
 Write-Host ""
-Write-Host "Checking Windows updates..." -ForegroundColor Green
+Write-Host "Check for Windows Updates..." -ForegroundColor Green
 Write-Host "------------------------------------" -ForegroundColor Green
 Install-Module -Name PSWindowsUpdate -Force
-Write-Host "Installing Windows updates..." -ForegroundColor Green
+Write-Host "Install Windows Updates..." -ForegroundColor Green
 Get-WindowsUpdate -AcceptAll -Install -ForceInstall -AutoReboot
 
 # Install complete
