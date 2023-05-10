990cb132fdabbf779f2f4860b30eb6e67cc7bd78
Jim Cronqvist <jim.cronqvist@gmail.com>
2023-05-10 16:01:19 +0200

    Update install-docker-host.sh

--- install-docker-host.sh
+++ install-docker-host.sh
@@ -317,62 +317,7 @@ EOF
         "10") # Install Kubernetes (K3s)
             
             # Install k3s
-            curl -sfL https://get.k3s.io | sh - 
-	    
-            # Change permission to make it readable by for example the ubuntu user, use with care as it gives non-root users access to the cluster
-            sudo chmod 644 /etc/rancher/k3s/k3s.yaml
-	    
-            # Activate the traefik dashboard internally on the private network (i.e. not use an exposed port like 80 or 443, we use port 9000) - available after next restart
-	    # https://github.com/traefik/traefik-helm-chart/blob/v23.0.1/traefik/values.yaml
-            sudo tee -a /var/lib/rancher/k3s/server/manifests/traefik-config.yaml << EOF
-apiVersion: helm.cattle.io/v1
-kind: HelmChartConfig
-metadata:
-  name: traefik
-  namespace: kube-system
-spec:
-  valuesContent: |-
-    dashboard:
-      enabled: true
-    ports:
-      traefik:
-        expose: true
-    logs:
-      access:
-        enabled: true
-EOF
-            
-            
-            # Display the kube config for remote usage
-            echo "" && echo "To connect remotely with kubectl, use this as your kube config (~/.kube/config): " && echo "" && sudo cat /etc/rancher/k3s/k3s.yaml | sed "s/127.0.0.1/$(hostname -I | cut -f1 -d' ')/g" && echo ""
-	    
-            # Check for Ready node, takes ~30 seconds before this command returns the 'correct' result.
-            kubectl get node
-            
-            # Install argoCD - install with a helm chart instead?
-            kubectl create namespace argocd
-            kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-            
-            # For Traefik as the ingress controller, the ArgoCD API server must run with TLS disabled.
-            echo -e "$(kubectl -n argocd get configmap argocd-cmd-params-cm -o yaml)\ndata:\n  server.insecure: \"true\"" | kubectl -n argocd apply -f -
-            
-            kubectl apply -f - <<EOF
-apiVersion: traefik.containo.us/v1alpha1
-kind: IngressRoute
-metadata:
-  name: argocd-server
-  namespace: argocd
-spec:
-  entryPoints:
-    - websecure
-  routes:
-    - kind: Rule
-      match: HostRegexp(\`argocd.{domain:[a-z0-9.]+}\`)
-      priority: 10
-      services:
-        - name: argocd-server
-          port: 80
-EOF
+            # @see install-k3s.sh
             
             ;;
         
