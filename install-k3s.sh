f422b3e67ec9a685d81b72c89e9b3ba0bb830539
Jim Cronqvist <jim.cronqvist@gmail.com>
2023-09-25 23:09:02 +0200

    Update install-k3s.sh

--- install-k3s.sh
+++ install-k3s.sh
@@ -63,72 +63,13 @@ cat << EOF | envsubst | sudo tee /etc/rancher/k3s/config.yaml
 write-kubeconfig-mode: "0644"
 EOF
 
+# Create .skip files to prevent the installation of a few k3s addons
+sudo mkdir -p /var/lib/rancher/k3s/server/manifests
+sudo touch /var/lib/rancher/k3s/server/manifests/traefik.yaml.skip
+sudo touch /var/lib/rancher/k3s/server/manifests/metrics-server.yaml.skip
+
 # Install k3s
 curl -sfL https://get.k3s.io | sh -
-
-
-# Activate the traefik dashboard internally on the private network (i.e. not use an exposed port like 80 or 443, we use port 9000) - available after the next restart
-# https://github.com/traefik/traefik-helm-chart/blob/v23.0.1/traefik/values.yaml
-sudo mkdir -p /var/lib/rancher/k3s/server/manifests/
-cat << EOF | envsubst | sudo tee /var/lib/rancher/k3s/server/manifests/traefik-config.yaml
-apiVersion: helm.cattle.io/v1
-kind: HelmChartConfig
-metadata:
-  name: traefik
-  namespace: kube-system
-spec:
-  valuesContent: |-
-    # Enable dashboard
-    ports:
-      websecure:
-        tls:
-          certResolver: letsencrypt-dnsmadeeasy
-          domains:
-            - main: ${DOMAIN}
-              sans:
-                - "*.${DOMAIN}"
-      traefik:
-        expose: true
-    dashboard:
-      enabled: true
-    
-    # Enable access logs
-    logs:
-      general:
-        # Format is required due to a bug in the helm chart for the specific version K3s is using at the time being.
-        format: common
-        # Set the debug level to debug temporarily to troubleshoot
-        #level: DEBUG
-      access:
-        enabled: true
-    
-    # Letsencrypt with DNSMadeEasy
-    deployment:
-      initContainers:
-        - name: volume-permissions
-          image: busybox:latest
-          command: ["sh", "-c", "chmod -Rv 600 /data/* || true"]
-          volumeMounts:
-          - name: data
-            mountPath: /data
-    certResolvers:
-      letsencrypt-dnsmadeeasy:
-        dnsChallenge:
-          provider: dnsmadeeasy
-          delaybeforecheck: 60
-        storage: /data/letsencrypt-dnsmadeeasy.json
-    env:
-      - name: DNSMADEEASY_API_KEY
-        valueFrom:
-          secretKeyRef:
-            name: dnsmadeeasy
-            key: apiKey
-      - name: DNSMADEEASY_API_SECRET
-        valueFrom:
-          secretKeyRef:
-            name: dnsmadeeasy
-            key: apiSecret
-EOF
 
 # Display the kube config for remote usage
 echo "" && echo "To connect remotely with kubectl, use this as your kube config (~/.kube/config): " && echo "" && sudo cat /etc/rancher/k3s/k3s.yaml | sed "s/127.0.0.1/$(hostname -I | cut -f1 -d' ')/g" && echo ""
@@ -137,10 +78,6 @@ sleep 40
 
 # Check for Ready node, takes ~30 seconds before this command returns the 'correct' result.
 kubectl get node
-
-# Create the letsencrypt dnsmadeeasy secret
-kubectl -n kube-system create secret generic dnsmadeeasy --from-literal=apiKey=${DNSMADEEASY_API_KEY} --from-literal=apiSecret=${DNSMADEEASY_API_SECRET}
-
 
 # Set up automated upgrades for K3s - install system-upgrade-controller
 kubectl apply -f https://github.com/rancher/system-upgrade-controller/releases/latest/download/system-upgrade-controller.yaml
