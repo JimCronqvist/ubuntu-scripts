82e19a1bed64306c3e7ae5e0549854f61e48d9ba
Jim Cronqvist <jim.cronqvist@gmail.com>
2023-09-27 22:50:34 +0200

    Create kubeswitch.sh

---
+++ kubeswitch.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+# Add it as a global executable
+# curl https://raw.githubusercontent.com/JimCronqvist/ubuntu-scripts/master/kubeswitch.sh | sudo tee /usr/local/bin/kubeswitch && sudo chmod +x /usr/local/bin/kubeswitch
+
+
+# 'select_option' from here: https://unix.stackexchange.com/a/415155
+function select_option {
+
+    # little helpers for terminal print control and key input
+    ESC=$( printf "\033")
+    cursor_blink_on()  { printf "$ESC[?25h"; }
+    cursor_blink_off() { printf "$ESC[?25l"; }
+    cursor_to()        { printf "$ESC[$1;${2:-1}H"; }
+    print_option()     { printf "   $1 "; }
+    print_selected()   { printf "  $ESC[7m $1 $ESC[27m"; }
+    get_cursor_row()   { IFS=';' read -sdR -p $'\E[6n' ROW COL; echo ${ROW#*[}; }
+    key_input()        {
+                         read -s -n3 key 2>/dev/null >&2
+                         if [[ $key = $ESC[A ]]; then echo up;    fi
+                         if [[ $key = $ESC[B ]]; then echo down;  fi
+                         if [[ $key = ""     ]]; then echo enter; fi
+                       }
+
+    # initially print empty new lines (scroll down if at bottom of screen)
+    for opt; do printf "\n"; done
+
+    # determine current screen position for overwriting the options
+    local lastrow=`get_cursor_row`
+    local startrow=$(($lastrow - $#))
+
+    # ensure cursor and input echoing back on upon a ctrl+c during read -s
+    trap "cursor_blink_on; stty echo; printf '\n'; exit" 2
+    cursor_blink_off
+
+    local selected=0
+    while true; do
+        # print options by overwriting the last lines
+        local idx=0
+        for opt; do
+            cursor_to $(($startrow + $idx))
+            if [ $idx -eq $selected ]; then
+                print_selected "$opt"
+            else
+                print_option "$opt"
+            fi
+            ((idx++))
+        done
+
+        # user key control
+        case `key_input` in
+            enter) break;;
+            up)    ((selected--));
+                   if [ $selected -lt 0 ]; then selected=$(($# - 1)); fi;;
+            down)  ((selected++));
+                   if [ $selected -ge $# ]; then selected=0; fi;;
+        esac
+    done
+
+    # cursor position back to normal
+    cursor_to $lastrow
+    printf "\n"
+    cursor_blink_on
+
+    return $selected
+}
+
+
+##
+## Script start
+##
+
+CURRENT_CONTEXT="$(kubectl config current-context)"
+CONTEXTS=( $(kubectl config get-contexts -o name | sort) )
+CONTEXTS_WITH_DEFAULT=("${CONTEXTS[@]}")
+
+for i in "${!CONTEXTS[@]}"; do
+    if [[ ${CONTEXTS[$i]} == "${CURRENT_CONTEXT}" ]]; then
+        CONTEXTS_WITH_DEFAULT[$i]="* ${CONTEXTS[$i]}"
+    fi
+done
+
+echo ""
+echo "Select a kubectl context:"
+echo ""
+
+select_option "${CONTEXTS_WITH_DEFAULT[@]}"
+INDEX=$?
+VALUE=${CONTEXTS[$INDEX]}
+
+kubectl config use-context "${VALUE}"
