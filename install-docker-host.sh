8f4ad3f6db921999ef7c31c5d007cb1a621c72ed
Jim Cronqvist <jim.cronqvist@gmail.com>
2025-05-13 00:54:19 +0200

    Update install-docker-host.sh

--- install-docker-host.sh
+++ install-docker-host.sh
@@ -422,14 +422,16 @@ EOF
             # Add kubeswitch
             curl -s https://raw.githubusercontent.com/JimCronqvist/ubuntu-scripts/master/kubeswitch.sh | sudo tee /usr/local/bin/kubeswitch >/dev/null && sudo chmod +x /usr/local/bin/kubeswitch
 
-            # Add Cilium CLI
-            CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/main/stable.txt)
-            CLI_ARCH=$(uname -m | grep -q aarch64 && echo arm64 || echo amd64)
-            curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
-            sha256sum --check cilium-linux-${CLI_ARCH}.tar.gz.sha256sum
-            sudo tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz /usr/local/bin
-            rm cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
-            cilium version --client
+            # Install Cilium CLI if not previously installed
+            if ! command -v cilium &> /dev/null; then
+                CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/main/stable.txt)
+                CLI_ARCH=$(uname -m | grep -q aarch64 && echo arm64 || echo amd64)
+                curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+                sha256sum --check cilium-linux-${CLI_ARCH}.tar.gz.sha256sum
+                sudo tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz /usr/local/bin
+                rm cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+                cilium version --client
+            fi
 
             ;;
             
