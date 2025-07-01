b8ccfe86d82f7117c334faaff252838c5a369153
Jim Cronqvist <jim.cronqvist@gmail.com>
2025-07-01 13:41:34 +0200

    Update install-docker-host.sh

--- install-docker-host.sh
+++ install-docker-host.sh
@@ -343,7 +343,17 @@ EOF
             ;;
         
         "11") # Install kubectl, helm, kustomize, eksctl, argocd cli, k9s, git, jq, yq
-        
+
+            # Install gh (GitHub CLI)
+            sudo mkdir -p -m 755 /etc/apt/keyrings \
+            && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+            && cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+            && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+            && sudo mkdir -p -m 755 /etc/apt/sources.list.d \
+            && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+            && sudo apt update \
+            && sudo apt install gh -y
+	
             # Install kubectl if not previously installed
             if ! command -v kubectl &> /dev/null; then
                 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
